b81611f2689d5614103f7ecb491b19eb483183f7
Efim Khlebnyy <Chicloon@gmail.com>
2016-01-26 16:04:02 +0900

    final version

--- App.jsx
+++ App.jsx
@@ -1,16 +1,43 @@
 // App component - represents the whole app
 App = React.createClass({
-  getTasks() {
-    return [
-      { _id: 1, text: "This is task 1" },
-      { _id: 2, text: "This is task 2" },
-      { _id: 3, text: "This is task 3" }
-    ];
-  },
+  mixins: [ReactMeteorData],
+  getMeteorData() {
+    let query = {};
+    if (this.state.hideCompleted) {
+      query = {checked: {$ne: true}};
+    }
+    return {
+      tasks: Tasks.find(query, {sort: {createdAt: -1}}).fetch(),
+      incompleteCount: Tasks.find({checked: {$ne: true}}).count(),
+      currentUser: Meteor.user()
 
+    };
+  },
+  getInitialState(){
+    return {
+      hideCompleted: false
+    }
+  },
   renderTasks() {
-    return this.getTasks().map((task) => {
-      return <Task key={task._id} task={task} />;
+    return this.data.tasks.map((task) => {
+      const currentUserId = this.data.currentUser && this.data.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+
+      return (<Task key={task._id} task={task}
+        showPrivateButton={showPrivateButton}/>
+      );
+    });
+  },
+  handleSubmit(event){
+    event.preventDefault();
+
+    var text = React.findDOMNode(this.refs.textInput).value.trim();
+    Meteor.call('addTask', text);
+    React.findDOMNode (this.refs.textInput).value = "";
+  },
+  toggleHideCompleted(){
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
     });
   },
 
@@ -18,7 +45,30 @@ App = React.createClass({
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.data.incompleteCount}) </h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly={true}
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted}
+            />
+            Hide Completed Tasks
+          </label>
+
+          <AccountsUIWrapper />
+
+          { this.data.currentUser ?
+            <form className='new-task'
+              onSubmit={this.handleSubmit}>
+              <input
+                type="text"
+                ref="textInput"
+                placeholder="Type to add new tasks" />
+            </form>
+          : ''
+          }
         </header>
 
         <ul>
--- react.jsx
+++ react.jsx
@@ -1,14 +1,69 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code is executed on the client only
+  Meteor.subscribe("tasks");
+
+  Accounts.ui.config({
+  passwordSignupFields: "USERNAME_ONLY"
+  });
 
   Meteor.startup(function() {
     // Use Meteor.startup to render the component after the page is ready
-    React.render( < App / > , document.getElementById("render-target"));
+    React.render(<App /> ,
+     document.getElementById("render-target"));
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function() {
+    Meteor.publish("tasks", function(){
+      return Tasks.find({
+        $or: [
+          {private: {$ne: true}},
+          {owner: this.userId}
+        ]
+      });
+    });
     // code to run on server at startup
   });
 }
+
+Meteor.methods({
+  addTask(text) {
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-autorized");
+    }
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+
+  removeTask(taskId){
+    const task = Tasks.findOne(taskId);
+    if(task.private && task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-autorized");
+    }
+    Tasks.remove(taskId);
+  },
+
+  setChecked(taskId, setChecked) {
+    const task = Tasks.findOne(taskId);
+    if(task.private && task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-autorized");
+    }
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  },
+
+  setPrivate(taskId, setToPrivate){
+    const task = Tasks.findOne(taskId);
+
+    if(task.owner !== Meteor.userId()){
+      throw new Meteor.Error("not-autorized");
+    }
+    Tasks.update(taskId, {$set: { private: setToPrivate}});
+  }
+});
