8437dc3b05cc8e3c46a416036f6968e45a0245d9
Chicloon <Chicloon@gmail.com>
2016-06-03 18:12:59 +0900

    Просто коммит

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component{
   render(){
     return (
-      <h1> Hello World </h1>
+      <h1> Hello Cruel World </h1>
     )
   }
 }
