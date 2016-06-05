5e409ddcc9a7e5743290a5cca02d77d875ffe853
Chicloon <Chicloon@gmail.com>
2016-06-05 21:33:01 +0900

    my commit

--- App.jsx
+++ App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default class App extends React.Component{
-  render(){
+export default class App extends React.Component {
+  render() {
     return (
-      <h1> Hello Cruel World </h1>
-    )
+      <h1> Hello, World </h1>
+    );
   }
 }
